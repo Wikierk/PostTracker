bc0d3284c4593568f22490d00f002d124153d55e
Jakub Pietrzykowski <j.pietrzykowski17@gmail.com>
2025-11-22 17:23:03 +0100

    feat: Implement PackageDetailsScreen, PackageOrderScreen for employee.

--- src/screens/employee/PackageDetailsScreen.tsx
+++ src/screens/employee/PackageDetailsScreen.tsx
@@ -1,22 +1,50 @@
-import React from "react";
-import { View, ScrollView, StyleSheet } from "react-native";
+import React, { useState } from "react";
+import { View, ScrollView, StyleSheet, Alert } from "react-native";
 import {
   Text,
+  Button,
+  Card,
   useTheme,
 } from "react-native-paper";
+import { NativeStackScreenProps } from "@react-navigation/native-stack";
+import { RootStackParamList } from "../../types/navigation";
+import { PackageStatus } from "../../types";
+import PackageStatusCard from "../../components/PackageStatusCard";
+import PackageDetailsCardProps from "../../components/PackageDetailsCard";
 
+type PackageDetailsScrenProps = NativeStackScreenProps<RootStackParamList, "PackageDetails">;
 
-const PackageDetailsScreen = () => {
-  const theme = useTheme()
+const PackageDetailsScreen = (props: PackageDetailsScrenProps) => {
+  const theme = useTheme();
+  const { packageData } = props.route.params;
+  const [status, setStatus] = useState<PackageStatus>(packageData.status);
+
+  const handleOrder = () => {
+    props.navigation.navigate("PackageOrder", { packageData: packageData })
+  };
+
   return (
     <ScrollView
       style={[styles.container, { backgroundColor: theme.colors.background }]}
     >
-      <Text>
-        To szczegoly paczki usera
-      </Text>
+      <Card style={styles.card}>
+        <PackageStatusCard status={status}/>
+      </Card>
+
+      {status !== "delivered" && (
+        <Button
+          mode="contained"
+          icon="hand-coin"
+          onPress={handleOrder}
+          style={styles.mainButton}
+          contentStyle={{ height: 50 }}
+        >
+          Odbierz przesyłkę
+        </Button>
+      )}
+      <PackageDetailsCardProps package={packageData}/>
     </ScrollView>
-  );
+  )
 };
 
 const styles = StyleSheet.create({
